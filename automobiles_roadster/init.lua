4a3383a17855ee91836b34cc31c63e1b059eccb8
Alexsandro Percy <alexsandro.percy@gmail.com>
2022-01-17 09:32:15 -0300

    ramp detection improved

--- automobiles_roadster/init.lua
+++ automobiles_roadster/init.lua
@@ -6,7 +6,7 @@ roadster.LONGIT_DRAG_FACTOR = 0.16*0.16
 roadster.LATER_DRAG_FACTOR = 30.0
 roadster.gravity = automobiles.gravity
 roadster.max_speed = 10
-roadster.max_acc_factor = 6
+roadster.max_acc_factor = 4
 roadster.max_fuel = 10
 
 roadster.front_wheel_xpos = 10.26
