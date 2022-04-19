222a81b99e5b24acebb44e1b346fb569d1a952dc
Alexsandro Percy <alexsandro.percy@gmail.com>
2022-04-19 18:55:51 -0300

    added support for max

--- automobiles_lib/init.lua
+++ automobiles_lib/init.lua
@@ -51,7 +51,7 @@ end
 function automobiles_lib.detect_player_api(player)
     local player_proterties = player:get_properties()
     local mesh = "character.b3d"
-    if player_proterties.mesh == mesh then
+    if player_proterties.mesh == mesh or player_proterties.mesh == "max.b3d" then
         local models = player_api.registered_models
         local character = models[mesh]
         if character then
